73ee60256155a91e2d19b0e1670808aa96f4080c
Daniel Galvão <daniel.galvao@justdigital.com.br>
2016-11-07 23:04:04 -0200

    Add angular controller to show cars

---
+++ www/js/controller.js
@@ -0,0 +1,15 @@
+angular.module('starter')
+.controller('ListController', function($scope){
+  $scope.carList = [
+    'BMW 120i',
+    'ONIX 1.6',
+    'Fiesta 2.0',
+    'C3 1.0',
+    'Uno Fire',
+    'Sentra 2.0',
+    'Astra Sedan',
+    'Vectra 2.0 Turbo',
+    'Hilux 4x4',
+    'Montana Cabine dupla'
+  ];
+});
