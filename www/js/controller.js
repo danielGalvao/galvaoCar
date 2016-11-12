8fe3a21379b72e05965b7942961c3ed75033aa8e
Daniel Galvão <galvaodev@gmail.com>
2016-11-12 01:10:14 -0200

    Fix ux

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,9 +1,17 @@
 angular.module('starter')
 .controller('ListController', function($scope){
-  $scope.carList = [{
-    'name': 'BMW 120i',
-    'price': 70000
-  }];
+  $scope.carList = [{"name" : "BMW 120i" , "price" : 70000},
+                    {"name" : "Onix 1.6" , "price" : 35000},
+                    {"name" : "Fiesta 2.0", "price" : 52000},
+                    {"name" : "C3 1.0", "price" : 22000},
+                    {"name" : "Uno Fire", "price" : 11000},
+                    {"name" : "Sentra 2.0", "price" : 53000},
+                    {"name" : "Astra Sedan", "price" : 39000},
+                    {"name" : "Vectra 2.0 Turbo", "price" : 37000},
+                    {"name" : "Hilux 4x4", "price" : 90000},
+                    {"name" : "Montana Cabine dupla", "price" : 57000},
+                    {"name" : "Outlander 2.4" ,"price" : 99000},
+                    {"name" : "Fusca 1500", "price" : 6000}];
 });
 
 angular.module('starter')
