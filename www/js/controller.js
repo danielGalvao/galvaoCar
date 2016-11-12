f8393360075c5e2b99222babf4e202d76c5a4bab
Daniel Galvão <galvaodev@gmail.com>
2016-11-12 00:11:25 -0200

    Add template car and price dynamic

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,20 +1,12 @@
 angular.module('starter')
 .controller('ListController', function($scope){
-  $scope.carList = [
-    'BMW 120i',
-    'ONIX 1.6',
-    'Fiesta 2.0',
-    'C3 1.0',
-    'Uno Fire',
-    'Sentra 2.0',
-    'Astra Sedan',
-    'Vectra 2.0 Turbo',
-    'Hilux 4x4',
-    'Montana Cabine dupla'
-  ];
+  $scope.carList = [{
+    'name': 'BMW 120i',
+    'price': 70000
+  }];
 });
 
 angular.module('starter')
 .controller('CarController', function($stateParams, $scope){
-  $scope.selectedCar = $stateParams.selectedCar;
+  $scope.selectedCar = angular.fromJson($stateParams.selectedCar);
 });
