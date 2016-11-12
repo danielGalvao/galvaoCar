08564dc2a9c1f1784ffee1e0864ac675034d44cf
Daniel Galvão <galvaodev@gmail.com>
2016-11-12 01:05:32 -0200

    Add accessory to add and calc total price

--- www/js/controller.js
+++ www/js/controller.js
@@ -8,5 +8,17 @@ angular.module('starter')
 
 angular.module('starter')
 .controller('CarController', function($stateParams, $scope){
+
   $scope.selectedCar = angular.fromJson($stateParams.selectedCar);
+  $scope.accessories = [{'name':'Freio ABS', 'price': 800},
+                        {'name':'Ar Condiconado', 'price': 1000},
+                        {'name':'MP3 Player', 'price': 500}
+                        ];
+  $scope.check = function(accessory, isChecked){
+    if(isChecked) {
+      $scope.selectedCar.price += accessory.price;
+    }else {
+      $scope.selectedCar.price -= accessory.price;
+    }
+  }
 });
